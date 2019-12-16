fef97fda79af1120707515ca65a03ce4912ed52f
Shiv Kushwah <shivendrakushwah@gmail.com>
2019-12-16 12:56:53 -0800

    allowing the creation of USMs

--- Src/Pc/CompilerCore/Backend/Prt/PrtCodeGenerator.cs
+++ Src/Pc/CompilerCore/Backend/Prt/PrtCodeGenerator.cs
@@ -1528,15 +1528,19 @@ namespace Plang.Compiler.Backend.Prt
                     } else {
                         context.Write(
                             output,
-                            $"PrtCloneValue(PrtMkInterface(context, {context.GetDeclNumber(ctorExpr.Interface)}, {ctorExpr.Arguments.Count}");
-                        foreach (IPExpr pExpr in ctorExpr.Arguments)
-                        {
-                            Debug.Assert(pExpr is IVariableRef);
-                            IVariableRef argVar = (IVariableRef)pExpr;
-                            context.Write(output, $", {GetVariableReference(function, argVar)}");
-                        }
+                            $"((_P_GEN_funval = P_CreateUSMMachineRequest_IMPL(context, _P_GEN_funargs)), (_P_GEN_funval))");
+                   
+                        // context.Write(
+                        //     output,
+                        //     $"PrtCloneValue(PrtMkInterface(context, {context.GetDeclNumber(ctorExpr.Interface)}, {ctorExpr.Arguments.Count}");
+                        // foreach (IPExpr pExpr in ctorExpr.Arguments)
+                        // {
+                        //     Debug.Assert(pExpr is IVariableRef);
+                        //     IVariableRef argVar = (IVariableRef)pExpr;
+                        //     context.Write(output, $", {GetVariableReference(function, argVar)}");
+                        // }
 
-                        context.Write(output, ")->id)");
+                        // context.Write(output, ")->id)");
                 
                     }
                     break;
